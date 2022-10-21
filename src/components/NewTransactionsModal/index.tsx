30965f84ececae6170193855b2b3925cdf2058d4
filipesaretta <filipessm124@gmail.com>
2022-10-21 11:13:54 -0300

    Add styles to modal

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,5 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { Content, Overlay } from './styles'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from './styles'
 
 export function NewTransactionsModal() {
   return (
@@ -8,7 +15,27 @@ export function NewTransactionsModal() {
       <Content>
         <Dialog.Title>New Transaction</Dialog.Title>
 
-        <Dialog.Close />
+        <CloseButton>
+          <X size={24} color="#7C7C8A" />
+        </CloseButton>
+
+        <form>
+          <input type="text" placeholder="Description" />
+          <input type="text" placeholder="Price" />
+          <input type="text" placeholder="Category" />
+
+          <TransactionType>
+            <TransactionTypeButton value="deposit" variant="deposit">
+              <ArrowCircleUp size={24} color="#00B37E" />
+              <span>Deposit</span>
+            </TransactionTypeButton>
+            <TransactionTypeButton value="withdraw" variant="withdraw">
+              <ArrowCircleDown size={24} color="#F75A68" />
+              <span>Withdraw</span>
+            </TransactionTypeButton>
+          </TransactionType>
+          <button type="submit">Register</button>
+        </form>
       </Content>
     </Dialog.Portal>
   )
--- src/components/NewTransactionsModal/styles.ts
+++ src/components/NewTransactionsModal/styles.ts
@@ -1,5 +1,7 @@
+/* eslint-disable prettier/prettier */
 import * as Dialog from '@radix-ui/react-dialog'
 import styled from 'styled-components'
+import * as RadioGroup from '@radix-ui/react-radio-group'
 
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
@@ -9,4 +11,97 @@ export const Overlay = styled(Dialog.Overlay)`
   background-color: rgba(0, 0, 0, 0.75);
 `
 
-export const Content = styled(Dialog.Content)``
+export const Content = styled(Dialog.Content)`
+  min-width: 32rem;
+
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  border-radius: 8px;
+  padding: 3rem;
+
+  background-color: ${(props) => props.theme['gray-800']};
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-top: 2rem;
+
+    input {
+      background-color: ${(props) => props.theme['gray-900']};
+      color: ${(props) => props.theme['gray-300']};
+      border: 1px solid ${(props) => props.theme['gray-900']};
+      padding: 1rem;
+      border-radius: 8px;
+    }
+
+    button[type='submit'] {
+      height: 3.625rem;
+      padding: 0.75rem 1.25rem;
+
+      background: ${(props) => props.theme['green-500']};
+      border-radius: 8px;
+      color: ${(props) => props.theme.white};
+      transition: background-color 0.3s;
+      border: none;
+
+      cursor: pointer;
+
+      &:hover {
+        background: ${(props) => props.theme['green-700']};
+      }
+    }
+  }
+`
+export const CloseButton = styled(Dialog.Close)`
+  position: absolute;
+  border: 0;
+  background-color: transparent;
+  right: 1.5rem;
+  top: 1.5rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  line-height: 0;
+`
+
+export const TransactionType = styled(RadioGroup.Root)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+`
+
+interface Props {
+  variant: 'deposit' | 'withdraw'
+}
+
+export const TransactionTypeButton = styled(RadioGroup.Item) <Props>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  height: 3.625rem;
+
+  background-color: ${(props) => props.theme['gray-700']};
+  color: ${(props) => props.theme['gray-300']};
+  border-radius: 8px;
+  border: 0;
+
+  svg {
+    color: ${(props) =>
+    props.variant === 'deposit' ? props.theme['green-700'] : '#F75A68'};
+  }
+
+  &[data-state='checked'] {
+    svg {
+      color: white;
+    }
+    color: white;
+    background-color: ${(props) =>
+    props.variant === 'deposit' ? props.theme['green-700'] : '#F75A68'};
+  }
+`
