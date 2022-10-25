0e3d764e847490223bc47b3eab05a721b6c4fee3
filipesaretta <filipessm124@gmail.com>
2022-10-25 18:28:11 -0300

    Create a hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,8 +1,11 @@
 import { SummaryCard, SummaryContainer } from './styles'
-
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { moneyFormatter } from '../../utils/Formatter'
+import { useSummary } from '../../hooks/useSummary'
 
 export function Summary() {
+  const summary = useSummary()
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -10,21 +13,21 @@ export function Summary() {
           <span>Revenue</span>
           <ArrowCircleUp size={32} color="#00B37E" />
         </header>
-        <strong>R$ 17.5000,00</strong>
+        <strong>{moneyFormatter.format(summary.income)}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
           <span>Expenses</span>
           <ArrowCircleDown size={32} color="#F75A68" />
         </header>
-        <strong>R$ 7.5000,00</strong>
+        <strong>{moneyFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
           <span>Profits</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
-        <strong>R$ 10.000,00</strong>
+        <strong>{moneyFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
